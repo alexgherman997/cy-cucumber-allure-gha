14a5c7ac9d596bedb55f833331dcd9dc030f0e5c
Alex Gherman <agherman@strategikonpharma.com>
2023-03-19 17:44:45 +0200

    cy cucumber allure gha

--- cypress.config.js
+++ cypress.config.js
@@ -1,41 +1,33 @@
-const { defineConfig } = require('cypress');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-const webpack = require('@cypress/webpack-preprocessor');
-const {
-    addCucumberPreprocessorPlugin
-} = require('@badeball/cypress-cucumber-preprocessor');
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+  allureWriter(on, config);
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents,
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false,
     env: {
-        issuePrefix: 'https://your.domain.atlassian.net/browse/',
-        tmsPrefix: 'https://some.testrail.instance/path/suite/caseID-'
+      allureReuseAfterSpec: true,
     },
-    e2e: {
-        setupNodeEvents: async function (on, config) {
-            await addCucumberPreprocessorPlugin(on, config);
-            on(
-                'file:preprocessor',
-                webpack({
-                    webpackOptions: {
-                        resolve: { extensions: ['.ts', '.js'] },
-                        module: {
-                            rules: [
-                                {
-                                    test: /\.feature$/,
-                                    use: [
-                                        {
-                                            loader: '@badeball/cypress-cucumber-preprocessor/webpack',
-                                            options: config
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                })
-            );
-            allureWriter(on, config);
-            return config;
-        }
-    }
+  },
 });
