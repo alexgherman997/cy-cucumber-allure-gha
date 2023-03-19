eff05f0da4f1e62f716502000a5d5a316896a75a
Alex Gherman <agherman@strategikonpharma.com>
2023-03-19 17:21:27 +0200

    tests cucumber allure gha

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,41 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const webpack = require('@cypress/webpack-preprocessor');
+const {
+    addCucumberPreprocessorPlugin
+} = require('@badeball/cypress-cucumber-preprocessor');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+        issuePrefix: 'https://your.domain.atlassian.net/browse/',
+        tmsPrefix: 'https://some.testrail.instance/path/suite/caseID-'
     },
-  },
+    e2e: {
+        setupNodeEvents: async function (on, config) {
+            await addCucumberPreprocessorPlugin(on, config);
+            on(
+                'file:preprocessor',
+                webpack({
+                    webpackOptions: {
+                        resolve: { extensions: ['.ts', '.js'] },
+                        module: {
+                            rules: [
+                                {
+                                    test: /\.feature$/,
+                                    use: [
+                                        {
+                                            loader: '@badeball/cypress-cucumber-preprocessor/webpack',
+                                            options: config
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                })
+            );
+            allureWriter(on, config);
+            return config;
+        }
+    }
 });
